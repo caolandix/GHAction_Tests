db5f0f582c785384fbf88b486134ecbb88f48eb4
Caolan O'Domhnaill <caolandix@users.noreply.github.com>
2026-08-03 11:24:47 -0700

    Update main.cpp
    Moving from basic C++ to Basic C++ with QT

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
-#include <iostream>
+#include <QApplication>
+#include <QPushButton>
 
-using namespace std;
+int main(int argc, char *argv[]) {
+    // 1. Initialize the application framework
+    QApplication app(argc, argv); 
 
-int main(int argc, char **argv) {
-    cout << "Hello, world" << endl;
+    // 2. Create a visual push button widget
+    QPushButton helloButton("Hello world!"); 
+    
+    // 3. Resize and show the widget on screen
+    helloButton.resize(200, 100);
+    helloButton.show(); 
+
+    // 4. Enter the main event loop and wait for user interaction
+    return app.exec(); 
 }
